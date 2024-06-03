4870f150514c108925ddff5a70fc758df86fc9c7
knilesh9 <neutron_stays@protonmail.com>
2024-06-03 14:52:40 +0530

    Some changes

--- nameRecursive.py
+++ nameRecursive.py
@@ -1,7 +1,10 @@
 count = 0
 
 def hello():
+    # don't forget to use global here since python uses local variables.And by using global you say check the global variable for this name.
     global count
+
+    # count ++ is invalid in python, I always mix this up
     count += 1
     print("Hi There ", count)
     hello()
